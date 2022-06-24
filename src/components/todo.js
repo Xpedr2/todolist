f42b1dd419c7da9a5eb4bbce8a045ed7319a9520
SantiagoS <santiagosanchezmon@gmail.com>
2022-06-24 18:43:26 -0300

    añado handleChange

--- src/components/todo.js
+++ src/components/todo.js
@@ -4,14 +4,20 @@ export default function TodoApp() {
     function handleClick(e){
         e.preventDefault();
         setTitle("Santi");
-
     }
+
+    function handleChange(e){
+        const value = e.target.value;
+
+        setTitle(value);
+    }
+
     const [title, setTitle] = useState("hola");
     return (
     <>
         <div className="todoContainer">
             <form className="todoCreateForm">
-                <input className="todoImput" value={title} />
+                <input onChange={handleChange} className="todoImput" value={title} />
                 <input onClick={handleClick} type="submit" value="Create todo" className="buttonCreate" />
                 {title}
                 </form>
