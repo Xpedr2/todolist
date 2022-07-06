2dc6319a4374b3d12e5b9137709d049d911a5001
SantiagoS <santiagosanchezmon@gmail.com>
2022-07-06 12:28:22 -0300

    cambios de estilo en login

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -2,11 +2,11 @@ import "./login.css"
 export default function Login () {
     return (
             <div className="loginContainer">
-                <p className="welcome">Welcome</p>
+                <p className="loginTittle">Welcome</p>
                 <label className="inputLabel">Username</label>
-                <input placeholder="Username"></input>
-                <label>Password</label>
-                <input type="password" placeholder="Password"></input>
+                <input className="loginInput" placeholder="Username"></input>
+                <label className="inputLabel">Password</label>
+                <input className="loginInput" type="password" placeholder="Password"></input>
                 <button className="buttonLogin">Login</button>
                 <button className="buttonGoogle">Sing in width Google</button>
                 <p>Forgot Password? <a className="loginLink">Click Here</a></p>
